70ef5fe940da3e67c508de42f9779b2fa116d6ca
50616046 <50616046@50616046-pc>
2017-12-11 13:39:17 +0900

    11/27課題

---
+++ src/thread/Thread_main.java
@@ -0,0 +1,37 @@
+package thread;
+
+public class Thread_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Thread_run []th = new Thread_run[th.length];
+			Thread_runnable []thr = new Thread_runnable[thr.length];
+			Thread_run[]start;
+			Thread_runnable[]run;
+			
+			try {
+				Thread_run[]join;
+				
+			}catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+			for(int i=0;i<10;i++) {
+				System.out.println("main‚©‚ço—Í : "+i);
+				sleep(500);
+			}
+	}
+
+	private static void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void sleep(int i) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	
+}
